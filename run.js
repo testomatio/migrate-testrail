0ea41c98f787846fcdfcec1bf5da87a45cd9dab6
Dmytro Pro <dmytro.am@gmail.com>
2024-10-09 22:05:39 +0300

    revert run.js to original

--- run.js
+++ run.js
@@ -1,36 +1,25 @@
 import 'dotenv/config'
 import migrateTestCases from './migrate.js';
-import {configureTestRail} from './testrail.js';
-import {configureTestomatio} from './testomatio.js';
+import { configureTestRail } from './testrail.js';
+import { configureTestomatio } from './testomatio.js';
 
-// Now you can access process.env.CF_ACCOUNT_ID wherever you need in your run.js logic
+// ENABLE THIS LINE TO RUN THE SCRIPT
+// PASS VALID VARIABLES TO ACCESS TESTRAIL
+// configureTestRail(testrailBaseUrl, username, password, projectId);
+configureTestRail(
+    process.env.TESTRAIL_URL,
+    process.env.TESTRAIL_USERNAME,
+    process.env.TESTRAIL_PASSWORD,
+    process.env.TESTRAIL_PROJECT_ID
+);
 
-export default {
-    async fetch(request, env, ctx) {
-        let accountId = env.CF_ACCOUNT_ID;
-        // Your existing logic in run.js
-        console.log(`Using Cloudflare account ID: ${accountId}`);
+// ENABLE THIS LINE TO RUN THE SCRIPT
+// PASS VALID VARIABLES TO ACCESS TESTOMATIO
+// configureTestomatio(testomatioAccessToken, testomatioHost, testomatioProject);
+configureTestomatio(
+    process.env.TESTOMATIO_TOKEN,
+    process.env.TESTOMATIO_HOST || 'https://app.testomat.io',
+    process.env.TESTOMATIO_PROJECT,
+);
 
-        // ENABLE THIS LINE TO RUN THE SCRIPT
-        // PASS VALID VARIABLES TO ACCESS TESTRAIL
-        // configureTestRail(testrailBaseUrl, username, password, projectId);
-        configureTestRail(
-            env.TESTRAIL_URL,
-            env.TESTRAIL_USERNAME,
-            env.TESTRAIL_PASSWORD,
-            env.TESTRAIL_PROJECT_ID
-        );
-
-        // ENABLE THIS LINE TO RUN THE SCRIPT
-        // PASS VALID VARIABLES TO ACCESS TESTOMATIO
-        // configureTestomatio(testomatioAccessToken, testomatioHost, testomatioProject);
-        configureTestomatio(
-            env.TESTOMATIO_TOKEN,
-            env.TESTOMATIO_HOST || 'https://app.testomat.io',
-            env.TESTOMATIO_PROJECT,
-        );
-
-        await migrateTestCases();
-
-    }
-}
+await migrateTestCases();
