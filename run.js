76c242449e3bdd8fb3361937be5dc6d24e3ec482
Dmytro Pro <dmytro.am@gmail.com>
2024-10-09 10:03:48 +0300

    export default

--- run.js
+++ run.js
@@ -1,31 +1,36 @@
 import 'dotenv/config'
 import migrateTestCases from './migrate.js';
-import { configureTestRail } from './testrail.js';
-import { configureTestomatio } from './testomatio.js';
+import {configureTestRail} from './testrail.js';
+import {configureTestomatio} from './testomatio.js';
 
 // Now you can access process.env.CF_ACCOUNT_ID wherever you need in your run.js logic
 const accountId = process.env.CF_ACCOUNT_ID;
 
-// Your existing logic in run.js
-console.log(`Using Cloudflare account ID: ${accountId}`);
+export default {
+    async fetch(request, env, ctx) {
+        // Your existing logic in run.js
+        console.log(`Using Cloudflare account ID: ${accountId}`);
 
-// ENABLE THIS LINE TO RUN THE SCRIPT
-// PASS VALID VARIABLES TO ACCESS TESTRAIL
-// configureTestRail(testrailBaseUrl, username, password, projectId);
-configureTestRail(
-  process.env.TESTRAIL_URL,
-  process.env.TESTRAIL_USERNAME, 
-  process.env.TESTRAIL_PASSWORD, 
-  process.env.TESTRAIL_PROJECT_ID
-);
+        // ENABLE THIS LINE TO RUN THE SCRIPT
+        // PASS VALID VARIABLES TO ACCESS TESTRAIL
+        // configureTestRail(testrailBaseUrl, username, password, projectId);
+        configureTestRail(
+            process.env.TESTRAIL_URL,
+            process.env.TESTRAIL_USERNAME,
+            process.env.TESTRAIL_PASSWORD,
+            process.env.TESTRAIL_PROJECT_ID
+        );
 
-// ENABLE THIS LINE TO RUN THE SCRIPT
-// PASS VALID VARIABLES TO ACCESS TESTOMATIO
-// configureTestomatio(testomatioAccessToken, testomatioHost, testomatioProject);
-configureTestomatio(
-  process.env.TESTOMATIO_TOKEN,
-  process.env.TESTOMATIO_HOST || 'https://app.testomat.io', 
-  process.env.TESTOMATIO_PROJECT,
-);
+        // ENABLE THIS LINE TO RUN THE SCRIPT
+        // PASS VALID VARIABLES TO ACCESS TESTOMATIO
+        // configureTestomatio(testomatioAccessToken, testomatioHost, testomatioProject);
+        configureTestomatio(
+            process.env.TESTOMATIO_TOKEN,
+            process.env.TESTOMATIO_HOST || 'https://app.testomat.io',
+            process.env.TESTOMATIO_PROJECT,
+        );
 
-await migrateTestCases();
+        await migrateTestCases();
+
+    }
+}
