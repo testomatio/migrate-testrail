804b045a68b010b47452208f14d51b3c33734baf
Dmytro Pro <dmytro.am@gmail.com>
2024-10-09 10:31:41 +0300

    env

--- run.js
+++ run.js
@@ -4,10 +4,10 @@ import {configureTestRail} from './testrail.js';
 import {configureTestomatio} from './testomatio.js';
 
 // Now you can access process.env.CF_ACCOUNT_ID wherever you need in your run.js logic
-const accountId = process.env.CF_ACCOUNT_ID;
 
 export default {
     async fetch(request, env, ctx) {
+        let accountId = env.CF_ACCOUNT_ID;
         // Your existing logic in run.js
         console.log(`Using Cloudflare account ID: ${accountId}`);
 
@@ -15,19 +15,19 @@ export default {
         // PASS VALID VARIABLES TO ACCESS TESTRAIL
         // configureTestRail(testrailBaseUrl, username, password, projectId);
         configureTestRail(
-            process.env.TESTRAIL_URL,
-            process.env.TESTRAIL_USERNAME,
-            process.env.TESTRAIL_PASSWORD,
-            process.env.TESTRAIL_PROJECT_ID
+            env.TESTRAIL_URL,
+            env.TESTRAIL_USERNAME,
+            env.TESTRAIL_PASSWORD,
+            env.TESTRAIL_PROJECT_ID
         );
 
         // ENABLE THIS LINE TO RUN THE SCRIPT
         // PASS VALID VARIABLES TO ACCESS TESTOMATIO
         // configureTestomatio(testomatioAccessToken, testomatioHost, testomatioProject);
         configureTestomatio(
-            process.env.TESTOMATIO_TOKEN,
-            process.env.TESTOMATIO_HOST || 'https://app.testomat.io',
-            process.env.TESTOMATIO_PROJECT,
+            env.TESTOMATIO_TOKEN,
+            env.TESTOMATIO_HOST || 'https://app.testomat.io',
+            env.TESTOMATIO_PROJECT,
         );
 
         await migrateTestCases();
